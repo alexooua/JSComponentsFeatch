25c2e60dc390c06cab0178cd3f377661bcdbbcd4
Заяц Алексей <alextotalk@gmail.com>
2020-11-14 20:32:30 +0200

    9-Reference type, mutability

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {User} from "./05-map+callback/06-callback";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <User />
   </React.StrictMode>,
   document.getElementById('root')
 );
